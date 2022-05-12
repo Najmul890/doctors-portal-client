e9df27e9a38b354d4841f99f252fc1c7d8c537ee
Najmul890 <najmul890890@gmail.com>
2022-05-12 09:12:10 -0700

    info section done in home page

---
+++ src/Pages/Home/Info/InfoCard/InfoCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InfoCard = ({img, title, bgClass}) => {
+    return (
+        <div class={`card lg:card-side bg-base-100 shadow-xl p-5  ${bgClass} `}>
+            <figure class="pl-5" ><img src={img} alt="Album"/></figure>
+            <div class="card-body">
+                <h2 class="card-title"> {title} </h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
